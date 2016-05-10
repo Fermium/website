665d8303b416a7240a1ca15c788c96d9b22170c3
Davide Bortolami <d@fermiumlabs.com>
2016-05-10 02:30:32 +0200

    deleted server list

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -6,6 +6,7 @@ DEPLOY_DIR_BASE="/var/www/html/"
 for SERVER in $SERVERS
 do
         # Initialize a new git repo in _site, and push it to our server.
+        echo "deploying on $SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
         cd _site
         git init
         git remote add deploy "deployer@$SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
@@ -14,5 +15,5 @@ do
         git add .
         git commit -m "Deploy"
         git push --force deploy master
-        slack_message.sh -t "Website deployed" -b "Server: $SERVER in dir: $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH " -c "repositories" -u "$SLACK_WEBHOOK_URL"
+        slack_message.sh -t "Website deployed" -b "Server $SERVER in dir $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH " -c "repositories" -u "$SLACK_WEBHOOK_URL"
 done
