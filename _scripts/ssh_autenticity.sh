4b58c9a77d0c4bf916950b1ec191070a2e47e22d
Davide Bortolami <d@fermiumlabs.com>
2016-05-10 02:39:13 +0200

    different for cicle

--- _scripts/ssh_autenticity.sh
+++ _scripts/ssh_autenticity.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Push codebase to the servers via rsync.
-for SERVER in $SERVERS
+for SERVER in "${SERVERS[@]}"
 do
+  echo $SERVER
   - ssh-keyscan -H $SERVER 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts && mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts
 done
