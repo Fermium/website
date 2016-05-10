d6c9a4fec58074ffebe779f022e278c61ab1d862
Davide Bortolami <d@fermiumlabs.com>
2016-05-10 15:21:12 +0200

    added simple txt report to every build

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-DEPLOY_DIR_BASE="/var/www/html/"
+DEPLOY_DIR_BASE="/var/www/html"
 DEPLOY_USER="deployer"
-
+DEPLOY_DIR="$DEPLOY_DIR_BASE/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
 
 # Push codebase to the servers via rsync.
 for SERVER in $(egrep -v '(^#|^\s*$|^\s*\t*#)' _scripts/server_list.txt)
@@ -10,8 +10,10 @@ do
         # Initialize a new git repo in _site, and push it to our server.
         echo "deploying on $SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
         cd _site
+        #write info about deployment
+        printf "This deploy was made on: \nServer\t$SERVER \nDirectory\t$DEPLOY_DIR \nCommit\t$TRAVIS_COMMIT \nBuild\t$TRAVIS_BUILD_NUMBER " > deploy-info.txt
         git init
-        git remote add deploy "$DEPLOY_USER@$SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
+        git remote add deploy "$DEPLOY_USER@$SERVER:$DEPLOY_DIR"
         git config user.name "Travis CI"
         git config user.email "webmaster@fermiumlabs.com"
         git add .
