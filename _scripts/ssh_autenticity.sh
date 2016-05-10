7a6aab415c7401d7f471b36c3c40ef34ff1f6bc1
Davide Bortolami <d@fermiumlabs.com>
2016-05-10 02:49:58 +0200

    directory fix

--- _scripts/ssh_autenticity.sh
+++ _scripts/ssh_autenticity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for SERVER in $(cat server_list.txt)
+for SERVER in $(cat _scripts/server_list.txt)
 do
   echo $SERVER
   ssh-keyscan -H $SERVER 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts && mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts
