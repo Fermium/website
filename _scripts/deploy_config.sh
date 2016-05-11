7dec67643180879103edc0c0c91869234eb6613c
Davide Bortolami <d@fermiumlabs.com>
2016-05-11 12:32:13 +0200

    changed nginx deploy report

--- _scripts/deploy_config.sh
+++ _scripts/deploy_config.sh
@@ -12,7 +12,7 @@ if [ "$(git log -n 1 --pretty=format:%h -- _configs/nginx/) " = "$(git log --pre
                 echo match
                 
                 #write info about deployment
-                printf "This configuration was deployed on: \nServer\t\t$SERVER \nDate\t\t$(date) \nCommit\t\t$TRAVIS_COMMIT \nBuild\t\t$TRAVIS_BUILD_NUMBER " > _configs/deploy-info.txt
+                printf "#This configuration was deployed on: \n#Server\t\t$SERVER \n#Date\t\t$(date) \n#Commit\t\t$TRAVIS_COMMIT \n#Build\t\t$TRAVIS_BUILD_NUMBER " > _configs/deploy-info.txt
                 
                 #Copy nginx configuration to config directory, overwriting 
                 scp -r _configs/* $DEPLOY_USER@$SERVER:$CONFIG_DIR_BASE
