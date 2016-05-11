c4e09d2508b96b93574d0f7fc14b8f4fb2ba6a4e
Davide Bortolami <d@fermiumlabs.com>
2016-05-11 13:31:29 +0200

    unified slack messaging

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,6 +5,7 @@ DEPLOY_DIR_BASE="/var/www/html"
 DEPLOY_USER="deployer"
 DEPLOY_DIR="$DEPLOY_DIR_BASE/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
 
+#DO NOT REMOVE THIS ECHO
 echo "Website deploy script"
 
 # Push codebase to the servers via rsync.
@@ -13,8 +14,9 @@ do
         # Initialize a new git repo in _site, and push it to our server.
         echo "deploying on $SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
         cd _site
-        #write info about deployment
+        #Print deployment report
         printf "This deploy was made on: \nServer\t\t$SERVER \nDate\t\t$(date) \nDirectory\t$DEPLOY_DIR \nCommit\t\t$TRAVIS_COMMIT \nBuild\t\t$TRAVIS_BUILD_NUMBER " > deploy-info.txt
+        
         git init
         git remote add deploy "$DEPLOY_USER@$SERVER:$DEPLOY_DIR"
         git config user.name "Travis CI"
@@ -23,6 +25,7 @@ do
         git commit -m "Deploying commit $TRAVIS_COMMIT of repository $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
         git push --force deploy master
         cd ..
-        slack_message.sh -t "Website deployed" -b "Successfully deployed website on server $SERVER in dir $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH from commit $TRAVIS_COMMIT. Check out http://$SERVER/deploy-info.txt" -c "repositories" -u "$SLACK_WEBHOOK_URL"
+        
+        printf "Successfully deployed website on server $SERVER in dir $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH from commit $TRAVIS_COMMIT. Check out http://$SERVER/deploy-info.txt\n\n" >> slack_message.txt
         
 done
