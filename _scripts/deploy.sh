541efb7fb01421b4173f155f3b85cda3f70c7a68
Davide Bortolami <d@fermiumlabs.com>
2016-05-11 12:16:30 +0200

    conditional deploy of nginx config

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-
 DEPLOY_DIR_BASE="/var/www/html"
-CONFIG_DIR_BASE="/var/www/config"
 DEPLOY_USER="deployer"
 DEPLOY_DIR="$DEPLOY_DIR_BASE/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
 
@@ -23,10 +21,6 @@ do
         git commit -m "Deploying commit $TRAVIS_COMMIT of repository $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
         git push --force deploy master
         cd ..
-        #Copy nginx configuration to config directory, overwriting 
-        scp -r _configs/* $DEPLOY_USER@$SERVER:$CONFIG_DIR_BASE
-        #safely reload nginx 
-        ssh -t $DEPLOY_USER@$SERVER 'sudo nginx -s reload' 
-        slack_message.sh -t "Website deployed" -b "Successfully deployed server $SERVER in dir $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH from commit $TRAVIS_COMMIT. Check out http://$SERVER/deploy-info.txt" -c "repositories" -u "$SLACK_WEBHOOK_URL"
+        slack_message.sh -t "Website deployed" -b "Successfully deployed website on server $SERVER in dir $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH from commit $TRAVIS_COMMIT. Check out http://$SERVER/deploy-info.txt" -c "repositories" -u "$SLACK_WEBHOOK_URL"
         
 done
