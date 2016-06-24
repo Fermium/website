0ea3dcdcd66940dda079d1ce1966f84df29233d4
Davide Bortolami <d@fermiumlabs.com>
2016-06-24 23:16:16 +0200

    BIG COMMIT, new scripts that allows a multi-website setup

--- _scripts/_tests/nginx/nginx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-nginx -t -c $TRAVIS_BUILD_DIR/_scripts/tests/nginx/test-conf
--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,29 +3,38 @@ set -e
 
 DEPLOY_DIR_BASE="/var/www/html"
 DEPLOY_USER="deployer"
-DEPLOY_DIR="$DEPLOY_DIR_BASE/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
 
 #DO NOT REMOVE THIS ECHO
 echo "Website deploy script"
 
-# Push codebase to the servers via rsync.
-for SERVER in $(egrep -v '(^#|^\s*$|^\s*\t*#)' _scripts/server_list.txt)
+
+#for every directory
+for WEBSITE in $(find . -maxdepth 1 -type d) 
 do
-        # Initialize a new git repo in _site, and push it to our server.
-        echo "deploying on $SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
-        cd _site
-        #Print deployment report
-        printf "This deploy was made on: \nServer\t\t$SERVER \nDate\t\t$(date) \nDirectory\t$DEPLOY_DIR \nCommit\t\t$TRAVIS_COMMIT \nBuild\t\t$TRAVIS_BUILD_NUMBER " > deploy-info.txt
+        DEPLOY_DIR="$DEPLOY_DIR_BASE/$WEBSITE/$TRAVIS_BRANCH"
         
-        git init
-        git remote add $SERVER "$DEPLOY_USER@$SERVER:$DEPLOY_DIR"
-        git config user.name "Travis CI"
-        git config user.email "webmaster@fermiumlabs.com"
-        git add .
-        git commit -m "Deploying commit $TRAVIS_COMMIT of repository $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
-        git push --force $SERVER master
-        cd ..
+        cd $TRAVIS_BUILD_DIR/Website/$WEBSITE
         
-        printf "Successfully deployed website on server $SERVER in dir $DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH from commit $TRAVIS_COMMIT. Check out http://$SERVER/deploy-info.txt\n\n" >> slack_message.txt
-        
+        #For every server
+        for SERVER in $(egrep -v '(^#|^\s*$|^\s*\t*#)' _scripts/server_list.txt)
+        do
+                
+                # Initialize a new git repo in _site, and push it to our server.
+                echo "deploying on $SERVER:$DEPLOY_DIR_BASE$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
+                cd _site
+                #Print deployment report
+                printf "This deploy was made on: \nServer\t\t$SERVER \nDate\t\t$(date) \nDirectory\t$DEPLOY_DIR \nCommit\t\t$TRAVIS_COMMIT \nBuild\t\t$TRAVIS_BUILD_NUMBER " > deploy-info.txt
+                
+                git init
+                git remote add $SERVER "$DEPLOY_USER@$SERVER:$DEPLOY_DIR"
+                git config user.name "Travis CI"
+                git config user.email "webmaster@fermiumlabs.com"
+                git add .
+                git commit -m "Deploying commit $TRAVIS_COMMIT of repository $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
+                git push --force $SERVER master
+                
+                printf "Successfully deployed website on server $SERVER in dir $DEPLOY_DIR from commit $TRAVIS_COMMIT. Check out http://$(cat address.txt)/deploy-info.txt\n\n" >> slack_message.txt
+                
+        done
+        cd 
 done
