8432fcddb4a6f7fda06182f1c2cd78f13ed88e7d
Davide Bortolami <d@fermiumlabs.com>
2016-06-25 03:49:25 +0200

    sudo

--- _scripts/tests/nginx/nginx.sh
+++ _scripts/tests/nginx/nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cp $TRAVIS_BUILD_DIR/_scripts/tests/nginx/test-conf /etc/nginx/conf.d/
+sudo cp $TRAVIS_BUILD_DIR/_scripts/tests/nginx/test-conf /etc/nginx/conf.d/
 sudo nginx -t 
