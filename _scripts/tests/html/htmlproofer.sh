29d30b50ac110122a97b71e9eb0ba7fb5fd5d2e1
Davide Bortolami <d@fermiumlabs.com>
2016-06-26 19:33:34 +0200

    more clear debug infos

--- _scripts/tests/html/htmlproofer.sh
+++ _scripts/tests/html/htmlproofer.sh
@@ -15,13 +15,13 @@ do
         #the site directory takes over _site if present.
         if [ -d "$WEBSITE_DIR/site" ]; then
                 # Control will enter here if $DIRECTORY exists.
-                cd $WEBSITE_DIR/site
+                WEBSITE_DIR=$WEBSITE_DIR/site
                 echo "directory site present, ignoring _site"
         else
                 echo "directory site not present, using _site"
-                cd $WEBSITE_DIR/_site
+                WEBSITE_DIR=$WEBSITE_DIR/_site
         fi 
         
-        bundle exec htmlproofer ./ --assume-extension --check-html --disable-external
-        bundle exec htmlproofer ./ --external_only        
+        bundle exec htmlproofer $WEBSITE_DIR --assume-extension --check-html --disable-external
+        bundle exec htmlproofer $WEBSITE_DIR --external_only        
 done
