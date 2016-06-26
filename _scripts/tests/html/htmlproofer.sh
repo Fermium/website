094888e303e04dbdf93f833e8c206531346f3fb5
Davide Bortolami <d@fermiumlabs.com>
2016-06-26 19:30:08 +0200

    serius testing script

--- _scripts/tests/html/htmlproofer.sh
+++ _scripts/tests/html/htmlproofer.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 set -e
 
-bundle exec htmlproofer ./_site --assume-extension --check-html --disable-external
-bundle exec htmlproofer ./_site --external_only
+echo "HTMLproofer test script"
+
+cd $TRAVIS_BUILD_DIR/Websites/ #dir that contains the websites in this repo
+#for every directory, max depth 1, clean the output
+for WEBSITE in $(find . -maxdepth 1 -type d | tr -d "./") 
+do
+        WEBSITE_DIR="$TRAVIS_BUILD_DIR/Websites/$WEBSITE"
+        cd $WEBSITE_DIR
+        
+        echo "#### testing website in folder $(pwd)"
+        
+        #the site directory takes over _site if present.
+        if [ -d "$WEBSITE_DIR/site" ]; then
+                # Control will enter here if $DIRECTORY exists.
+                cd $WEBSITE_DIR/site
+                echo "directory site present, ignoring _site"
+        else
+                echo "directory site not present, using _site"
+                cd $WEBSITE_DIR/_site
+        fi 
+        
+        bundle exec htmlproofer ./ --assume-extension --check-html --disable-external
+        bundle exec htmlproofer ./ --external_only        
+done
