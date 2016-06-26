36bcd83d35f76549dfb4a69b288d6d1609dd5099
Davide Bortolami <d@fermiumlabs.com>
2016-06-26 20:44:18 +0200

    updated script to not use address.txt anymore

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -16,14 +16,18 @@ do
         DEPLOY_DIR="$DEPLOY_DIR_BASE/$WEBSITE/$TRAVIS_BRANCH"
         WEBSITE_DIR="$TRAVIS_BUILD_DIR/Websites/$WEBSITE"
         
+        
         cd $WEBSITE_DIR
-        echo "#### deploying website in folder $(pwd)"
+        #replacing "_" with "." in directory name to obtain web address 
+        WEBSITE_ADDR=$(echo "$WEBSITE_DIR" | sed 's@.*/@@' | tr -s '_' '.')
 
+        echo "#### deploying website $WEBSITE_ADDR in folder $(pwd)"
+        
         #For every server, ignore #comments
         for SERVER in $(egrep -v '(^#|^\s*$|^\s*\t*#)' $TRAVIS_BUILD_DIR/_scripts/server_list.txt)
         do
                 
-                echo "#### deploying $WEBSITE on $SERVER:$DEPLOY_DIR"
+                echo "#### deploying $WEBSITE_ADDR on $SERVER:$DEPLOY_DIR"
                 
                 #the site directory takes over _site if present.
                 if [ -d "$WEBSITE_DIR/site" ]; then
@@ -37,7 +41,7 @@ do
                 
                 
                 #Print deployment report
-                printf "This deploy was made on: \nServer\t\t$SERVER \nDate\t\t$(date) \nDirectory\t$DEPLOY_DIR \nCommit\t\t$TRAVIS_COMMIT \nBuild\t\t$TRAVIS_BUILD_NUMBER " > deploy-info.txt
+                printf "This deploy was made on: \nServer\t\t$SERVER \nThis website is: \nWebsite\t\t$WEBSITE_ADDR \nDate\t\t$(date) \nDirectory\t$DEPLOY_DIR \nCommit\t\t$TRAVIS_COMMIT \nBuild\t\t$TRAVIS_BUILD_NUMBER " > deploy-info.txt
                 
                 #push the repository
                 git init
@@ -49,7 +53,7 @@ do
                 git push --force $SERVER master
                 
                 # concat a slack message report for our team
-                printf "Successfully deployed $WEBSITE on server $SERVER in dir $DEPLOY_DIR from commit $TRAVIS_COMMIT. Check out http://$(cat address.txt)/deploy-info.txt\n\n" >> $TRAVIS_BUILD_DIR/slack_message.txt
+                printf "Successfully deployed $WEBSITE on server $SERVER in dir $DEPLOY_DIR from commit $TRAVIS_COMMIT. Check out http://$WEBSITE_ADDR/deploy-info.txt\n\n" >> $TRAVIS_BUILD_DIR/slack_message.txt
                 
         done
         cd 
