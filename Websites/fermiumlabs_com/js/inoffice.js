f90d2b04b3390b9e55fb7fd8609b3e5cbf01790f
s <s@fermiumlabs.com>
2016-08-08 10:41:28 -0400

    major fixes to contact page

---
+++ Websites/fermiumlabs_com/js/inoffice.js
@@ -0,0 +1,16 @@
+function get_request(who) {
+  $.get("http://public-check-in.office.fermiumlabs.com/look/"+who+"_in_ufficio", function(response){
+    if (response.status){
+      $('#'+who+" .overlay-status").removeClass('not-in-office');
+      $('#'+who+" .overlay-status").addClass('in-office');
+    }
+    else{
+      $('#'+who+" .overlay-status").removeClass('in-office');
+      $('#'+who+" .overlay-status").addClass('not-in-office');
+    }
+    setTimeout( get_request, 120000 );
+  });
+}
+
+get_request('d'); 
+get_request('s');
