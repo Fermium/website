69cca4bb94bd7fbdc5c75c2640bc4449cf786474
Simone Tosato <s@one.dev.fermiumlabs.com-1470316151061-512mb-nyc3-01>
2016-08-12 10:05:33 -0400

    fixing contact us page

--- Websites/fermiumlabs_com/Assets/js/custom.js
+++ Websites/fermiumlabs_com/Assets/js/custom.js
@@ -6,13 +6,13 @@ if(isMobile){
     }
   });
   $('.front').removeClass('front');
-  $('.back').removeClass('back');  
-  $('.flipper').removeClass('flipper');  
-  $('.flip-container').removeClass('flip-container'); 
-  
-  
-  
-  
+  $('.back').removeClass('back');
+  $('.flipper').removeClass('flipper');
+  $('.flip-container').removeClass('flip-container');
+
+
+
+
   var lis = $(".collaborator");
   if(lis.length%2!=0){
     lis.last().append('<div class="collaborator col col-sm-6 col-md-2 col-centered flip-container" ontouchstart="this.classList.toggle(\'hover\');"></div>');
@@ -21,11 +21,18 @@ if(isMobile){
   for(var i = 0; i < lis.length; i+=2) {
       lis.slice(i, i+2).wrapAll("<div class='row row-centered flex collab-wrapper'></div>");
   }
-  $(".collab-wrapper").unwrap();  
+  $(".collab-wrapper").unwrap();
 }
 
 
 $(".former").submit(function(){
-  event.preventDefault();
-  $(this).submit();
+  $.post("//websitephp7scripts.fermiumlabs.com/contactuszendesk.php",{z_subject: $("#z_subject").val(),z_description: $("#z_description").val(),z_name: $("#z_name").val(),z_requester: $("#z_requester").val()})
+  .done(function(){
+    
+  })
+  .fail(function(){
+    alert("failed");
+  });
+
+  return false;
 });
